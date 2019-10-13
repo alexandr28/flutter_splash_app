bace88eb3a7fbeaa623f7f4f9599c2489a9527d4
Alexandr Campos <alexandr.campos.rios@gmail.com>
2019-10-13 12:56:17 -0500

    example splash app

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/alexandrcampos/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/alexandrcampos/Proyectos/Flutter/flutter_splash_app"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/alexandrcampos/flutter/bin/cache/artifacts/engine/ios"
